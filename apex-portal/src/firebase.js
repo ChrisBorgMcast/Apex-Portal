fef83788c7017e2609b951f28a4e7524611d118c
Christian Borg <christian.borg.g52707@mcast.edu.mt>
2026-05-08 15:09:54 +0200

    Security: Move Firebase credentials to environment variables

--- apex-portal/src/firebase.js
+++ apex-portal/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzRmI6gMAPPv_OmK9bSK9SE3KV5LpfAzU",
-  authDomain: "apex-portal-6f65b.firebaseapp.com",
-  projectId: "apex-portal-6f65b",
-  storageBucket: "apex-portal-6f65b.firebasestorage.app",
-  messagingSenderId: "592734054478",
-  appId: "1:592734054478:web:1613048b06df673eafbb1f",
-  measurementId: "G-6W4THZ3P6Q"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
